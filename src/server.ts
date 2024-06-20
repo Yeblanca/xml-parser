66f92b752845e87753d181fc14946bbda78bae02
Yeblanca <yp@yeblanca.com>
2024-06-19 23:49:11 -0500

    Graphql server

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,78 @@
-// server.ts
 import express from 'express';
+import { buildSchema } from 'graphql';
+import { createHandler } from 'graphql-http/lib/use/express';
+import { PrismaClient } from '@prisma/client';
 import { fetchAndProcessMakes } from './services/xml-parser.service';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const prisma = new PrismaClient();
+let dataInitialized = true; // Flag to track if data has been initialized, more elegant options can be implemented.
 
-// Function that starts the server.
-const startServer = () => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-};
+console.log('Database URL:', process.env.DATABASE_URL);
 
-// Start server only after data is done being fetched and processed
-const initializeDataAndServer = async () => {
-  try {
-    console.log('Fetching and processing makes...');
-    await fetchAndProcessMakes();
-    console.log('Data fetched and processed successfully');
-    startServer();
-  } catch (error) {
-    console.error('Error during initialization', error);
-    process.exit(1); // Exit the process with a failure code
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type MakeVehicleTypes {
+    typeId: String
+    typeName: String
+  }
+
+  type Make {
+    id: String
+    makeId: String
+    makeName: String
+    vehicleTypes: [MakeVehicleTypes]
+  }
+
+  type Query {
+    makes(first: Int, skip: Int): [Make]
+    vehicleTypesByMake(makeId: String!): [MakeVehicleTypes]
+  }
+`);
+
+const root = {
+  async makes({ first = 10, skip = 0 }) {
+    if (!dataInitialized) {
+      await fetchAndProcessMakes();
+      dataInitialized = true;
+    }
+    return await prisma.make.findMany({
+      take: first,
+      skip: skip,
+      include: {
+        vehicleTypes: true
+      }
+    });
+  },
+  async vehicleTypesByMake({ makeId }: { makeId: string }) {
+    console.log(makeId);
+    const results = await prisma.make.findMany({
+      where: {
+        makeId: makeId
+      },
+      include: {
+        vehicleTypes: true
+      }
+    });
+    return results[0].vehicleTypes;
   }
 };
 
-// Initialize data and then start the server
-initializeDataAndServer();
+const app = express();
+
+// Create and use the GraphQL handler
+app.all(
+  '/graphql',
+  createHandler({
+    schema,
+    rootValue: root
+  })
+);
+
+// Start the server at port
+app.listen(4000, async () => {
+  if (!dataInitialized) {
+    await fetchAndProcessMakes();
+    dataInitialized = true;
+  }
+  console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+});
